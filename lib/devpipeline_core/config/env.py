6df5e9f219bdacb3d9265385055f86efceb50df0
Stephen Newell <sjnewell@gmail.com>
2018-11-26 22:41:11 -0700

    Work on #4 - Migrate configuration modifiers out of core
    Partial support for the new environment configuration modification.
    Simple tests work.

--- lib/devpipeline_core/config/env.py
+++ lib/devpipeline_core/config/env.py
@@ -3,26 +3,26 @@
 """Functionality related to environment configuration"""
 
 import os
-import re
-
-_ENV_PATTERN = re.compile(R"env.(\w+)")
 
 
-def get_env_list(config_map):
-    """
-    Retrieve a list of environemnt variables a configuration will modify.
+def _prepend_env(config, base_key, current_value):
+    prepend_key = "env.{}.prepend".format(base_key)
+    if prepend_key in config:
+        prepend_value = os.pathsep.join(config.get_list(prepend_key))
+        if current_value is not None:
+            return "{}{}{}".format(prepend_value, os.pathsep, current_value)
+        return prepend_value
+    return current_value
 
-    The list of variables will include anything in a configuration (including
-    in extra configuration files) that can alter the environment.  The list
-    will include only the variable names, not the env prefix.
 
-    Arguments
-    config_map - A configuration map.
-    """
-    env_adjustments = {}
-    #for source in _SOURCE_FUNCTIONS:
-        #source(config_map, env_adjustments)
-    return env_adjustments.keys()
+def _append_env(config, base_key, current_value):
+    append_key = "env.{}.append".format(base_key)
+    if append_key in config:
+        append_value = os.pathsep.join(config.get_list(append_key))
+        if current_value is not None:
+            return "{}{}{}".format(current_value, os.pathsep, append_value)
+        return append_value
+    return current_value
 
 
 def create_environment(config_map):
@@ -32,24 +32,21 @@ def create_environment(config_map):
     Arguments
     config_map - A configuration map.
     """
-    def _apply_override(adjustment, ret):
-        upper_key = adjustment.upper()
-        initial_value = config_map["current_config"].get(
-            "env.{}".format(adjustment))
-        if not initial_value:
-            initial_value = ret.get(upper_key)
-        new_value = devpipeline_core.config.modifier.modify_everything(
-            initial_value, config_map,
-            "env.{}".format(adjustment), os.pathsep)
-        if new_value:
-            ret[upper_key] = new_value
+    ret = os.environ.copy()
+    current_config = config_map["current_config"]
+    for env in current_config.get_list("dp.env_list"):
+        real_env = env.upper()
+        value = os.environ.get(real_env)
+        value = _prepend_env(current_config, env, value)
+        value = _append_env(current_config, env, value)
+        if value is not None:
+            ret[real_env] = value
         else:
-            ret.pop(upper_key, None)
-
-    env_adjustments = get_env_list(config_map)
-    if env_adjustments:
-        ret = os.environ.copy()
-        for adjustment in env_adjustments:
-            _apply_override(adjustment, ret)
-        return ret
-    return os.environ
+            # either an override or erase
+            key = "env.{}".format(env)
+            if key in current_config:
+                ret[real_env] = os.pathsep.join(current_config.get_list(key))
+            else:
+                if real_env in ret:
+                    del ret[real_env]
+    return ret
