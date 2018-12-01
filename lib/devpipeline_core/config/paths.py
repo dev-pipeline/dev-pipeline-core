737677991e4170180f944b35dcde8951b49ff93a
Stephen Newell <sjnewell@gmail.com>
2018-12-01 09:51:23 -0700

    Work on #4 - Migrate config modifiers from core

--- lib/devpipeline_core/config/paths.py
+++ lib/devpipeline_core/config/paths.py
@@ -18,25 +18,3 @@ def _make_path(base_dir, *endings):
     if not base_dir:
         base_dir = _get_config_dir()
     return os.path.join(base_dir, *endings)
-
-
-def _override_base_dir(config_map):
-    if "config_dir" in config_map:
-        return config_map["config_dir"]
-    return None
-
-
-def get_overrides_root(base_dir=None, config_map=None):
-    """
-    Get the root path for override files.
-
-    Arguments
-    base_dir - The base directory to search in.
-    config_map - A configuration map.  If provided, this will override
-                 base_dir.
-    """
-    if config_map:
-        base_dir = _override_base_dir(config_map)
-    return _make_path(base_dir, "overrides.d")
-
-
